7f23f2b55c917d76f75c5de660b0b1bad8d6888f
Adrianna Kralka <92793178+akralka@users.noreply.github.com>
2022-07-27 17:20:07 +0200

    Update działka.c

--- lab_5/działka.c
+++ lab_5/działka.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf("Enter size of the field :\n ");
+    scanf("%d", &n);
+    short T[100][100];
+    printf("Enter array: \n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &T[i][j]);
+        }
+
+    }
+    int a = 0;      // odstep pionowo
+    int b = 0;      // odstep poziomo
+    int f, s;       // f - flaga(potrzebna jest) ,   s - pole powierzchni
+    int max = 0;        // maksymalne pole powierzchni
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(T[i][j] == 1){
+                continue;
+            }
+            a = -1;     // bo tak
+            b = 0;
+            while(i + a < n-1){
+                a++;
+                b = 0;
+                f = 1;
+                int p;      // jedziemy po wierszach w nowych kolumnach
+                while(f == 1 && j + b <= n-1){
+                    for(p = 0; p <= a; p++){
+                        if(T[i+p][j+b] == 1){
+                            f = 0;      // ups, nieuzytkowy, wracaaamy
+                            b -= 1;
+                            break;
+                        }
+                    }
+                    if(j+b == n-1){
+                        f = 0;      // bez tego petla nie dziala
+                    }
+                    if(f == 1 && j + b < n-1){
+                        b++;        // nastepna kolumna do sprawdzenia
+                    }
+                }
+                s = (a+1)*(b+1);        //liczymy pole
+                if(s > max){
+                    max = s;
+                }
+            }
+        }
+    }
+    printf("%d", max);
+}
