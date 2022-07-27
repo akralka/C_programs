570a6bedcf3c7755371b429a615c5f482c3a9e07
Adrianna Kralka <92793178+akralka@users.noreply.github.com>
2022-07-27 17:17:20 +0200

    Create spirala.c

---
+++ lab_4/spirala.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of the array: \n");
+    scanf("%d", &n);
+    int tab[n][n];
+    int k = 1;
+    for (int p =0; p<(n/2 +1); p++){  // p to jest liczba warstw gotowych
+
+    for (int i = p; i < n-p; i++) {
+        tab[p][i] = k;
+        k++;
+        }
+
+        for (int i = p + 1; i < n-p; i++) {
+                tab[i][n-p-1] = k;
+                k++;
+            }
+
+        for (int i =n-p-2; i >= p; i--) {
+                tab[n-p-1][i] = k;
+                k++;
+            }
+        for (int i = n-p-2; i>= p+1; i--) {
+                tab[i][p] = k;
+                k++;
+        }}
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d", tab[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+//for(int i = 0; i<n; i++){
+//for(int j = 0; j<n; j++){
+//tab[i][j] = n * i + j;
+//printf("%3d", tab[i][j]);
+//}
+//printf("\n");
+
+//    for(int i = 0; i < n*n; i++){
+//        printf("%d ", *((int*)tab +i));
+//    printf("\n");
+//    }
+// to by poszło liniowo
