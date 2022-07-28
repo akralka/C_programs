e741e6668217a0e6f3cba37e30a39f9a4a2ed4f9
Adrianna Kralka <92793178+akralka@users.noreply.github.com>
2022-07-28 15:59:51 +0200

    Update Print_array.c

--- lab_3/Print_array.c
+++ lab_3/Print_array.c
@@ -31,3 +31,44 @@ int main(){
     return 0;
 }
 
+
+
+// zmodyfikowane
+// #include <stdio.h>
+
+
+// int main() {
+//     int tab[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int k;
+//     int n;
+//     int order;
+
+//     // pobieramy od uzytkownika indeks pierwszego elementu, dlugosc tabeli i kolejnosc
+//     printf("Enter index of the first number:\n");
+//     scanf("%d", &k);
+//     printf("Enter the length of the table:\n");
+//     scanf("%d", &n);
+//     printf("Enter 0 for ascending order or 1 for descending order: \n");
+//     scanf("%d", &order);
+//     printf("Our table: \n");
+
+//     // sprawdzamy czy uzytkownik chce tabele rosnaca czy malejaca i ja printujemy
+//     if(order == 0){
+//         for(int* i=tab+k; i < tab+n+1; i++){
+//             printf("%d ", *i);
+//         }
+//         for(int* i=tab; i< tab+k; i++){
+//             printf("%d ", *i);
+//         }
+//     }
+//     else if(order == 1){
+//         for(int* i=tab+k; i >= tab; i--){
+//             printf("%d ", *i);
+//         }
+//         for(int* i=tab+n; i > tab+k; i--){
+//             printf("%d ", *i);
+//         }
+//     }
+//     return 0;
+// }
+
