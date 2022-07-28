99636c24e1a79a12ffc0ca146824be69c10a28b5
Adrianna Kralka <92793178+akralka@users.noreply.github.com>
2022-07-28 16:05:04 +0200

    Update działka.c

--- lab_5/działka.c
+++ lab_5/działka.c
@@ -14,30 +14,30 @@ int main() {
     }
     int a = 0;      // odstep pionowo
     int b = 0;      // odstep poziomo
-    int f, s;       // f - flaga(potrzebna jest) ,   s - pole powierzchni
+    int f, s;       // f - flaga ,   s - pole powierzchni
     int max = 0;        // maksymalne pole powierzchni
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             if(T[i][j] == 1){
                 continue;
             }
-            a = -1;     // bo tak
+            a = -1;     
             b = 0;
             while(i + a < n-1){
                 a++;
                 b = 0;
                 f = 1;
-                int p;      // jedziemy po wierszach w nowych kolumnach
+                int p;      // idziemy po wierszach w nowych kolumnach
                 while(f == 1 && j + b <= n-1){
                     for(p = 0; p <= a; p++){
                         if(T[i+p][j+b] == 1){
-                            f = 0;      // ups, nieuzytkowy, wracaaamy
+                            f = 0;      // nieuzytkowy, wracamy
                             b -= 1;
                             break;
                         }
                     }
                     if(j+b == n-1){
-                        f = 0;      // bez tego petla nie dziala
+                        f = 0;      
                     }
                     if(f == 1 && j + b < n-1){
                         b++;        // nastepna kolumna do sprawdzenia
